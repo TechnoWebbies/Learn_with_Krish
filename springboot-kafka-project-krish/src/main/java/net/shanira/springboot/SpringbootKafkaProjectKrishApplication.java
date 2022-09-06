11562ae005278cbabc1d584aa655c60d56490dd8
Shanira Perera <97827742+TechnoWebbies@users.noreply.github.com>
2022-09-06 11:10:12 +0530

    practical implementation of Async MS using Kafka

---
+++ springboot-kafka-project-krish/src/main/java/net/shanira/springboot/SpringbootKafkaProjectKrishApplication.java
@@ -0,0 +1,13 @@
+package net.shanira.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaProjectKrishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaProjectKrishApplication.class, args);
+	}
+
+}
---
+++ springboot-kafka-project-krish/src/test/java/net/shanira/springboot/SpringbootKafkaProjectKrishApplicationTests.java
@@ -0,0 +1,13 @@
+package net.shanira.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaProjectKrishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
